2a8f8e1d66c2b3aee2b1795455b46cee4d986ea0
ShahadathAlam <shahadathalam@ymail.com>
2024-06-16 21:01:08 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -123,14 +123,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ element }) {
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && element}
+      {isOpen && children}
     </div>
   );
 }
